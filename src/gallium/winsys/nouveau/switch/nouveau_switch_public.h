a9ae86602d771274a647caf8c76800e831f24440
fincs <fincs.alt1@gmail.com>
2018-09-16 16:54:22 +0200

    nouveau_switch_winsys: add nouveau_switch_resource_get_syncpoint()

--- src/gallium/winsys/nouveau/switch/nouveau_switch_public.h
+++ src/gallium/winsys/nouveau/switch/nouveau_switch_public.h
@@ -3,7 +3,9 @@
 #define __NOUVEAU_SWITCH_PUBLIC_H__
 
 struct pipe_screen;
+struct pipe_resource;
 
 struct pipe_screen *nouveau_switch_screen_create(void);
+int nouveau_switch_resource_get_syncpoint(struct pipe_resource *resource, unsigned int *out_threshold);
 
 #endif
